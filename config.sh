3a79f2e7aa9937a60504148de2b161286ca36e16
Jean Cabral <jeancabral@gmail.com>
2021-12-15 12:57:26 -0300

    chore: Remove sentinels

--- config.sh
+++ config.sh
@@ -11,9 +11,7 @@ cat <<EOF >./config.json
   "address": "$ADDRESS",
   "redis_host": "$REDIS_HOST",
   "redis_port":"$REDIS_PORT",
-  "redis_sentinels": "$REDIS_SENTINELS",
   "redis_db": "$REDIS_DB",
-  "redis_master_name": "$REDIS_MASTER_NAME",
   "namespaces": [
     {
       "name": "public",
